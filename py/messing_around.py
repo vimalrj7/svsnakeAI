8f5ac01f3ee7c2b2b8c020fb4143efc1d9fda8d9
vimalrj7 <vimalrj@hotmail.com>
2020-03-29 22:09:33 -0400

    finished Hamiltonian

--- py/messing_around.py
+++ py/messing_around.py
@@ -4,24 +4,27 @@ def get_hamiltonian_coordinate(board_size, coordinate):
     if x > 0 and x < board_size - 1 and y == 0:
         x -= 1
     
-    if x % 2 == 0:
-        if y == board_size -1:
-            x += 1
-        else:
-            y += 1
+    else: 
+        if x % 2 == 0:
+            if y == board_size -1:
+                x += 1
+            else:
+                y += 1
 
-    if x % 2 == 1:
-        #at top and not in the last column, take a right
-        if y == 1 and x != board_size - 1:
-            x += 1
-        #at the last column but not in top corner, go up
-        elif x == board_size - 1 and y != 0:
-            y -= 1
-        #at the last column in the top corner, go left
-        elif x == board_size -1 and y == 0:
-            x -=1
         else:
-            y -= 1
+            #at top and not in the last column, take a right
+            if y == 1 and x != board_size - 1:
+                x += 1
+            #at the last column but not in top corner, go up
+            elif x == board_size - 1 and y != 0:
+                y -= 1
+            #at the last column in the top corner, go left
+            elif x == board_size -1 and y == 0:
+                x -=1
+            else:
+                y -= 1
+
     return [x, y]
+
 
 print(get_hamiltonian_coordinate(4, [0,0]))
