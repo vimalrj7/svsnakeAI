28082c4426356d3b8c3b1837c06d82987d7722f4
Sameer Bharatia <58922791+sameerbharatia@users.noreply.github.com>
2020-04-07 03:07:48 -0400

    All code formatted. :)

--- py/hamiltonian.py
+++ py/hamiltonian.py
@@ -1,22 +1,38 @@
 class hamiltonian:
+    '''
+    All the functions required to run a hamiltonian cycle and perform hamiltonian operations.
+    '''
     def __init__(self, board_size):
+        '''
+        Need the board_size to initalize the class.
+        (int) -> None
+        '''
         self.board_size = board_size
 
     def get_coordinate(self, coordinate):
+        '''
+        Gets the next hamiltonian coordinate in the cycle based on the current one.\n
+        (int[2]) -> int[2]
+        '''
+        
         x, y = coordinate[0], coordinate[1]
 
+        #the final horizontal stretch going back to the origin
         if x > 0 and x < self.board_size - 1 and y == 0:
             x -= 1
         
-        else: 
+        else:
+            #even columns 
             if x % 2 == 0:
+                #shift into the next column
                 if y == self.board_size - 1:
                     x += 1
+                #move up the column
                 else:
                     y += 1
 
             else:
-                #at top and not in the last column, take a right
+                #at row the before the top and not in the last column, take a right
                 if y == 1 and x != self.board_size - 1:
                     x += 1
                 #at the last column but not in top corner, go up
@@ -25,21 +41,42 @@ class hamiltonian:
                 #at the last column in the top corner, go left
                 elif x == self.board_size -1 and y == 0:
                     x -=1
+                #move down the column otherwise
                 else:
                     y -= 1
 
         return [x, y]
 
     def get_number(self, coordinate):
-        start, number = [0, 0], 0        
+        '''
+        Gets the hamiltonian number for a coordinate on the board.\n
+        (int[2]) -> int
+        '''
+        
+        #initialize start and number to zero
+        start, number = [0, 0], 0 
+
+        #keep getting next coordinate and adding 1 to the hamiltonian number
+        #until the start reaches the coordinate entered
         while start != coordinate:
             start = self.get_coordinate(start)
             number += 1
+
+        #return the resulting number
         return number
 
     def get_distance(self, head_coords, item_coords):
+        '''
+        Gets the hamiltonian distance between the head and another coordinate on the board.\n
+        (int[2], int[2]) -> int
+        '''
+
+        #convert the coordinates to their respective hamiltonian number
         head = self.get_number(head_coords)
         item = self.get_number(item_coords)
+
+        #returns the hamiltonian distance based on the location of the head
+        #relative to the item to account for wrap around options
         if head <= item:
             return item - head
         elif head > item:
--- py/main.py
+++ py/main.py
@@ -6,6 +6,7 @@ if __name__ == "__main__":
     #ptr to board
     board = init_board()
     
+    #initialize values
     play_on = 1
     show_board(board)
     axis = AXIS_INIT
@@ -20,20 +21,30 @@ if __name__ == "__main__":
         x_tail, y_tail = board[0].snek[0].tail[0].coord[x], \
                         board[0].snek[0].tail[0].coord[y]
 
+        #head and tail coordinates
         head = [x_head, y_head]
         tail = [x_tail, y_tail]
         
+        #get food coordinates
         food = get_food_coordinates(BOARD_SIZE, board[0].cell_value)
+        #get the current frame
         curr_frame = get_curr_frame()
+        #find the next move in coordinate form
         next_move = choose_move(head, tail, food, BOARD_SIZE, board[0], CYCLE_ALLOWANCE, curr_frame)
+        #convert the move to an axis and direction pair
         axis, direction = convert_to_move(head, next_move)
 
+        #moves the game forward one frame
         play_on = advance_frame(axis, direction, board)
         
-        if board[0].snek[0].length > 40 and board[0].snek[0].length < 46:
-            show_board(board)
-            sleep(0.1)
-            
+        #if board[0].snek[0].length > 40 and board[0].snek[0].length < 46:
+        
+        #displays the board
+        show_board(board)
+        #creates delay
+        sleep(0.1)
+        
+        #checks if the snake won, notifies player if won and ends the game
         if board[0].snek[0].length == BOARD_SIZE**2 - 1:
             play_on = 0
             print("=================")
